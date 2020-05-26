bf65da708d0fd527e8629742461c5895a3b23c1d
seanngpack <seanngpack@gmail.com>
2020-05-26 14:31:46 -0400

    added arduino class

---
+++ include/model/arduino/Arduino.h
@@ -0,0 +1,21 @@
+#ifndef SWAG_SCANNER_ARDUINO_H
+#define SWAG_SCANNER_ARDUINO_H
+
+#include <iostream>
+
+namespace arduino {
+    class Arduino {
+    public:
+
+        Arduino();
+
+
+    private:
+        std::string UART_SERVICE_UUID = "5ffba521-2363-41da-92f5-46adc56b2d37";
+        std::string ROTATE_TABLE_CHAR_UUID = "5ffba522-2363-41da-92f5-46adc56b2d37";
+        std::string TABLE_POSITION_CHAR_UUID = "5ffba523-2363-41da-92f5-46adc56b2d37";
+        std::string IS_TABLE_ROTATING_CHAR_UUID = "5ffba524-2363-41da-92f5-46adc56b2d37";
+    };
+}
+
+#endif //SWAG_SCANNER_ARDUINO_H
---
+++ src/model/arduino/Arduino.cpp
@@ -0,0 +1,5 @@
+#include "Arduino.h"
+
+arduino::Arduino Arduino() {
+
+}
