2457a56429835f541f56d492bc824b5eca7cbb5b
seanngpack <seanngpack@gmail.com>
2020-09-11 00:08:54 -0400

    fixed positioning with view grid

--- src/view/Visualizer.cpp
+++ src/view/Visualizer.cpp
@@ -121,6 +121,58 @@ void visual::Visualizer::compareVis(const std::shared_ptr<pcl::PointCloud<pcl::P
     }
 }
 
+void visual::Visualizer::compareVisFour(const std::shared_ptr<pcl::PointCloud<pcl::PointXYZ>> &cloud1,
+                                        const std::shared_ptr<pcl::PointCloud<pcl::PointXYZ>> &cloud2,
+                                        const std::shared_ptr<pcl::PointCloud<pcl::PointXYZ>> &cloud3,
+                                        const std::shared_ptr<pcl::PointCloud<pcl::PointXYZ>> &cloud4,
+                                        const std::string &cloud1_desc,
+                                        const std::string &cloud2_desc,
+                                        const std::string &cloud3_desc,
+                                        const std::string &cloud4_desc) {
+    pcl::visualization::PCLVisualizer viewer("Compare 4 clouds");
+    viewer.initCameraParameters();
+
+    int v1(0);
+    viewer.createViewPort(0.0, 0.5, 0.5, 1., v1);
+    viewer.setBackgroundColor(0, 0, 0, v1);
+    viewer.addText(cloud1_desc, 10, 10, "cloud 1", v1);
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> handler1(cloud1, 255, 255, 255);
+    viewer.addPointCloud<pcl::PointXYZ>(cloud1, handler1, "sample cloud1", v1);
+
+    int v2(1);
+    viewer.createViewPort(0.5, 0.5, 1.0, 1.0, v2);
+    viewer.setBackgroundColor(0.3, 0.3, 0.3, v2);
+    viewer.addText(cloud2_desc, 10, 10, "cloud 2", v2);
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> handler2(cloud2, 255, 255, 255);
+    viewer.addPointCloud<pcl::PointXYZ>(cloud2, handler2, "sample cloud2", v2);
+
+    int v3(2);
+    viewer.createViewPort(0, 0.0, .5, 0.5, v3);
+    viewer.setBackgroundColor(0.3, 0.3, 0.3, v3);
+    viewer.addText(cloud3_desc, 10, 10, "cloud 3", v3);
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> handler3(cloud3, 255, 255, 255);
+    viewer.addPointCloud<pcl::PointXYZ>(cloud3, handler3, "sample cloud3", v3);
+
+    int v4(3);
+    viewer.createViewPort(0.5, 0.0, 1.0, 0.5, v4);
+    viewer.setBackgroundColor(0.3, 0.3, 0.3, v4);
+    viewer.addText(cloud4_desc, 10, 10, "cloud 4", v4);
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> handler4(cloud4, 255, 255, 255);
+    viewer.addPointCloud<pcl::PointXYZ>(cloud4, handler4, "sample cloud4", v4);
+
+    viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud1");
+    viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud2");
+    viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud3");
+    viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud4");
+//    viewer->addCoordinateSystem(1.0);
+
+    while (!viewer.wasStopped()) {
+        viewer.spinOnce(100);
+        std::this_thread::sleep_for(100ms);
+    }
+
+}
+
 void visual::Visualizer::normalsVis(const std::shared_ptr<pcl::PointCloud<pcl::PointXYZ>> &cloud,
                                     const std::shared_ptr<pcl::PointCloud<pcl::Normal>> &normal) {
     pcl::visualization::PCLVisualizer viewer("3D viewer");
