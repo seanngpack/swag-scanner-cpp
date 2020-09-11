950249b1fda70fa21a0a6812cd61f35545401ffe
seanngpack <seanngpack@gmail.com>
2020-09-10 20:24:05 -0400

    added functionality for data testing files to be copied over to build target

--- test/research/DepthFiltering/CompareDepthFiltering.cpp
+++ test/research/DepthFiltering/CompareDepthFiltering.cpp
@@ -1,6 +1,13 @@
 #include <gtest/gtest.h>
-#include "Model.h"
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <filesystem>
+#include <iostream>
 #include "Visualizer.h"
+#include "Model.h"
+
+namespace fs = std::filesystem;
 
 class CompareDepthFilteringFixture : public ::testing::Test {
 
@@ -19,6 +26,18 @@ protected:
     }
 };
 
+/**
+ * This test does a side-by-side of a scans of the calibration fixture. The filtered scan has default values:
+ * "decimation_magnitude": 2,
+    "spatial_filter_magnitude": 1,
+    "spatial_smooth_alpha": 0.45,
+    "spatial_smooth_delta": 5
+ */
 TEST_F(CompareDepthFilteringFixture, CompareCalFixture) {
-
+    std::cout << "Current path is " << fs::current_path() << '\n';
+    std::shared_ptr<pcl::PointCloud<pcl::PointXYZ>> cloud_raw(new pcl::PointCloud<pcl::PointXYZ>);
+    std::shared_ptr<pcl::PointCloud<pcl::PointXYZ>> cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::io::loadPCDFile<pcl::PointXYZ>(fs::current_path().string() + "/research/DepthFiltering/data/fixture_default_raw.pcd", *cloud_raw);
+    pcl::io::loadPCDFile<pcl::PointXYZ>(fs::current_path().string() + "/research/DepthFiltering/data/fixture_default_filtered.pcd", *cloud_filtered);
+    viewer->compareVis(cloud_raw, cloud_filtered);
 }
