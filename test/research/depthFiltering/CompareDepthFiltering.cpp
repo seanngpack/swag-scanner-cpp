886edacc25765ac2831441f427769269cc6ff660
seanngpack <seanngpack@gmail.com>
2020-09-25 19:58:20 -0400

    added filtering test

--- test/research/depthFiltering/CompareDepthFiltering.cpp
+++ test/research/depthFiltering/CompareDepthFiltering.cpp
@@ -1,3 +1,5 @@
+#define private public
+
 #include <gtest/gtest.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
@@ -7,7 +9,8 @@
 #include <filesystem>
 #include <iostream>
 #include "Visualizer.h"
-#include "ScanModel.h"
+#include "Plane.h"
+#include "CalibrationModel.h"
 #include "Constants.h"
 
 namespace fs = std::filesystem;
@@ -15,11 +18,11 @@ namespace fs = std::filesystem;
 class CompareDepthFilteringFixture : public ::testing::Test {
 
 protected:
-    model::ScanModel *mod;
+    model::CalibrationModel *mod;
     visual::Visualizer *viewer;
 
     virtual void SetUp() {
-        mod = new model::ScanModel();
+        mod = new model::CalibrationModel();
         viewer = new visual::Visualizer();
     }
 
@@ -73,6 +76,12 @@ TEST_F(CompareDepthFilteringFixture, CompareCalFixture) {
  * Results so far: Seems like realsense filtering vastly outperforms pcl bilateral filtering.
  * fixture_raw is raw cloud
  *
+ *
+ * findings: small sigmar_r keeps the edge -> increase r increase filter
+ * small sigma_s = less filtering, more edge -> decrease s increase filter
+ * im getting less angle error using this pipeline -> crop -> voxel -> bilateral
+ * calibration does better with less filtering
+ *
  * Doesn't seem like bilateral filtering does a very good job,
  *
  * fixture_1 is realsense filtered cloud
@@ -86,25 +95,54 @@ TEST_F(CompareDepthFilteringFixture, CompareRealsensePCL) {
     auto fixture_1 = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
     auto fixture_2 = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
     auto fixture_3 = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
-    pcl::io::loadPCDFile<pcl::PointXYZ>(fs::current_path().string() + "/research/depthFiltering/data/fixture_raw.pcd",
+    pcl::io::loadPCDFile<pcl::PointXYZ>(fs::current_path().string() + "/research/depthFiltering/data/raw_cloud.pcd",
                                         *fixture_raw);
-    pcl::io::loadPCDFile<pcl::PointXYZ>(fs::current_path().string() + "/research/depthFiltering/data/fixture_1.pcd",
+    pcl::io::loadPCDFile<pcl::PointXYZ>(fs::current_path().string() + "/research/depthFiltering/data/calibration_cloud.pcd",
                                         *fixture_1);
 
     std::cout << fixture_1->size() << std::endl;
 
+    mod->get_calibration_planes_coefs(fixture_1, false);
+
 
     *fixture_2 = *fixture_raw;
     mod->crop_cloud(fixture_2, cal_min_x, cal_max_x, cal_min_y, cal_max_y, cal_min_z, cal_max_z);
-    mod->voxel_grid_filter(fixture_2, .0005);
-    mod->bilateral_filter(fixture_2, 50, .05);
+    mod->bilateral_filter(fixture_2, 20, .01);
+    mod->voxel_grid_filter(fixture_2, .001);
+    mod->get_calibration_planes_coefs(fixture_2, false);
     std::cout << fixture_2->size() << std::endl;
     *fixture_3 = *fixture_raw;
-    mod->bilateral_filter(fixture_3, 1, .01);
     mod->crop_cloud(fixture_3, cal_min_x, cal_max_x, cal_min_y, cal_max_y, cal_min_z, cal_max_z);
+    mod->bilateral_filter(fixture_3, 10, .001);
     mod->voxel_grid_filter(fixture_3, .001);
+    mod->get_calibration_planes_coefs(fixture_3, false);
 
     fixture_1 = mod->crop_cloud_cpy(fixture_1, cal_min_x, cal_max_x, cal_min_y, cal_max_y, cal_min_z, cal_max_z);
+
+    viewer->compareVisFour(fixture_raw, fixture_1, fixture_2, fixture_3);
+}
+
+/**
+ * Compare bilateral filtering against full-resolution clouds to find optimal settings.
+ */
+TEST_F(CompareDepthFilteringFixture, TestBilateralFilterFullResolution) {
+    using namespace constants;
+    std::cout << "Current path is " << fs::current_path() << '\n';
+    auto fixture_raw = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
+    auto fixture_1 = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
+    auto fixture_2 = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
+    auto fixture_3 = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
+    pcl::io::loadPCDFile<pcl::PointXYZ>(fs::current_path().string() + "/research/depthFiltering/data/sponge.pcd",
+                                        *fixture_raw);
+    mod->crop_cloud(fixture_raw, cal_min_x, cal_max_x, cal_min_y, cal_max_y, cal_min_z, cal_max_z);
+    std::cout << fixture_raw->size() << std::endl;
+
+    *fixture_1 = *fixture_raw;
+    mod->bilateral_filter(fixture_1, 10, .01);
+    *fixture_2 = *fixture_raw;
+    mod->bilateral_filter(fixture_2, 10, .005);
+    *fixture_3 = *fixture_raw;
+    mod->bilateral_filter(fixture_3, 10, .001);
 
     viewer->compareVisFour(fixture_raw, fixture_1, fixture_2, fixture_3);
 }
--- test/research/depthFiltering/README.md
+++ test/research/depthFiltering/README.md
@@ -4,6 +4,7 @@ In this experiment, I aimed to compare realsense's ```spatial edge-preserving fi
 PCL's ```FastBilateralFilter```. Tuning different parameters and treating the clouds
 through decimation filters, realsense's implementation clearly shows better results than PCL's implementation.
 
-oh wait, nvm, let's just get new calibration scans lol
+So far, I have found that minimal bilateral filtering leads to best plane detection results,
+achieving only .179% error on this dataset.
 
 ![section opening and closing](example.gif)
